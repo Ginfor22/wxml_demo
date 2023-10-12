c029ff12803b941311a162e8e992600c90b3f5a1
Ginfor22 <2480396479@qq.com>
2023-10-12 09:54:15 +0800

    note

---
+++ 项目代码、数据库备份或数据文件/user.sql
@@ -0,0 +1,59 @@
+/*
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 5.6.22-log : Database - shixun
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`shixun` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `shixun`;
+
+/*Table structure for table `register` */
+
+DROP TABLE IF EXISTS `register`;
+
+CREATE TABLE `register` (
+  `username` varchar(255) NOT NULL,
+  `pwd` int(11) NOT NULL,
+  `zhanghao` varchar(255) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `register` */
+
+insert  into `register`(`username`,`pwd`,`zhanghao`) values ('123456',123456,'乌拉');
+
+/*Table structure for table `user_info` */
+
+DROP TABLE IF EXISTS `user_info`;
+
+CREATE TABLE `user_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `pwd` varchar(100) NOT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `gender` int(1) DEFAULT NULL,
+  `age` int(3) DEFAULT NULL,
+  `school` varchar(50) DEFAULT NULL,
+  `department` varchar(50) DEFAULT NULL,
+  `introduce` varchar(500) DEFAULT NULL,
+  `img_path` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user_info` */
+
+insert  into `user_info`(`id`,`username`,`pwd`,`name`,`gender`,`age`,`school`,`department`,`introduce`,`img_path`) values (2,'123456','123456',NULL,NULL,12,NULL,NULL,NULL,NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
